1edeeb86bb4db84560a2ee20a5d3968c90da880a
Daniel Clarke <u8sand@gmail.com>
2023-08-03 19:28:22 -0400

    Update client url

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 })
 
