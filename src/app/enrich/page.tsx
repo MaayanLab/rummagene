20d80c8f2a3a78cfc498164ef044783ecf66c706
Daniel Clarke <u8sand@gmail.com>
2023-08-04 08:07:27 -0400

    Click to view genesets

--- src/app/enrich/page.tsx
+++ src/app/enrich/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react'
 import client from '@/lib/apollo-client'
 import { useEnrichmentQueryQuery, useFetchUserGeneSetQuery } from "@/graphql"
 import ensureArray from "@/utils/ensureArray"
@@ -24,20 +25,32 @@ export default function Enrich({
       genes: ensureArray(userGeneSet?.userGeneSet?.genes).filter((gene): gene is string => !!gene).map(gene => gene.toUpperCase()),
     }
   })
+  const [modelGeneSet, setModelGeneSet] = React.useState<{ description: string, genes: string[] }>()
   return (
     <>
-      <div className="flex flex-row gap-2">
-        <span className="prose">Description</span>
-        <span className="prose underline cursor-pointer">{userGeneSet?.userGeneSet?.description || 'Gene set'} ({userGeneSet?.userGeneSet?.genes?.length ?? '?'} genes)</span>
+      <div className="flex flex-row gap-2 alert">
+        <span className="prose">Description:</span>
+        <label
+          htmlFor="geneSetModal"
+          className="prose underline cursor-pointer"
+          onClick={evt => {
+            setModelGeneSet({
+              genes: (userGeneSet?.userGeneSet?.genes ?? []).filter((gene): gene is string => !!gene),
+              description: userGeneSet?.userGeneSet?.description || 'Gene set',
+            })
+          }}
+        >{userGeneSet?.userGeneSet?.description || 'Gene set'} ({userGeneSet?.userGeneSet?.genes?.length ?? '?'} genes)</label>
+
       </div>
       <progress className={classNames("progress w-full", { 'hidden': !userGeneSetLoading && !enrichmentResultsLoading })}></progress>
       <div className="flex flex-row flex-wrap">
         {enrichmentResults?.geneSetLibraries?.nodes.map((geneSetLibrary, i) => (
-          <div key={i} className="card">
-            <div className="card-body">
-              <h2 className="card-title">
-                {geneSetLibrary.name}
-              </h2>
+          <div key={i} className="collapse collapse-arrow">
+            <input type="checkbox" defaultChecked /> 
+            <h2 className="collapse-title text-xl font-medium">
+              {geneSetLibrary.name}
+            </h2>
+            <div className="collapse-content overflow-x-auto">
               <table className="table table-xs">
                 <thead>
                   <tr>
@@ -52,19 +65,52 @@ export default function Enrich({
                   {geneSetLibrary.enrichLibraryBackground.nodes.map((enrichmentResult, j) => (
                     <tr key={j}>
                       <th>{enrichmentResult.geneSet?.term ?? null}</th>
-                      <td className="text-underline cursor-pointer">{enrichmentResult.overlapGenes.nodes.length}</td>
-                      <td>{enrichmentResult.oddsRatio?.toPrecision(3)}</td>
-                      <td>{enrichmentResult.pvalue?.toPrecision(3)}</td>
-                      <td>{enrichmentResult.adjPvalue?.toPrecision(3)}</td>
+                      <td className="whitespace-nowrap text-underline cursor-pointer">
+                        <label
+                          htmlFor="geneSetModal"
+                          className="prose underline cursor-pointer"
+                          onClick={evt => {
+                            setModelGeneSet({
+                              genes: enrichmentResult.overlapGenes.nodes.map(gene => gene.symbol) ?? [],
+                              description: enrichmentResult.geneSet?.term ?? '',
+                            })
+                          }}
+                        >{enrichmentResult.overlapGenes.nodes.length}</label>
+                      </td>
+                      <td className="whitespace-nowrap">{enrichmentResult.oddsRatio?.toPrecision(3)}</td>
+                      <td className="whitespace-nowrap">{enrichmentResult.pvalue?.toPrecision(3)}</td>
+                      <td className="whitespace-nowrap">{enrichmentResult.adjPvalue?.toPrecision(3)}</td>
                     </tr>
                   ))}
-                  <tr></tr>
                 </tbody>
               </table>
-              <p></p>
             </div>
           </div>
         )) ?? null}
+      </div>
+      <input
+        type="checkbox"
+        id="geneSetModal"
+        className="modal-toggle"
+        onChange={evt => {
+          if (!evt.currentTarget.checked) {
+            setModelGeneSet(undefined)
+          }
+        }}
+      />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">
+            {modelGeneSet?.description}
+          </h3>
+          <textarea
+            className="w-full"
+            readOnly
+            rows={8}
+            value={modelGeneSet?.genes.join('\n') ?? ''}
+          />
+        </div>
+        <label className="modal-backdrop" htmlFor="geneSetModal">Close</label>
       </div>
     </>
   )
