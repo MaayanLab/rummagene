f10c180c7b50a809afe29def4795ef9348fa39ef
Daniel Clarke <u8sand@gmail.com>
2023-08-04 13:23:36 -0400

    Turn ingest script into helper with ingest/clean subcommands

--- helper.py
+++ helper.py
@@ -159,12 +159,54 @@ def import_gene_set_library(
 
   return gene_set_library
 
-@click.command()
+@click.group()
+def cli(): pass
+
+@cli.command()
+@click.option('-i', '--input', type=click.Path(exists=True, file_okay=True, path_type=Path), help='GMT file to clean')
+@click.option('-o', '--output', type=click.Path(path_type=Path), help='Output location')
+def clean(input, output):
+  import re
+  from maayanlab_bioinformatics.harmonization.ncbi_genes import ncbi_genes_lookup
+
+  lookup = None
+  def gene_lookup(value):
+    ''' Don't allow pure numbers or spaces--numbers can typically match entrez ids
+    '''
+    if type(value) != str: return None
+    if re.search(r'\s', value): return None
+    if re.match(r'\d+(\.\d+)?', value): return None
+    global lookup
+    if lookup is None:
+      lookup = ncbi_genes_lookup(filters=lambda ncbi: ncbi)
+    return lookup(value)
+
+  terms = set()
+  with input.open('r') as fr:
+    with output.open('w') as fw:
+      for line in filter(None, map(str.strip, fr)):
+        term, _, *geneset = line.split('\t')
+        geneset_mapped = [gene_mapped for gene in geneset for gene_mapped in (gene_lookup(gene),) if gene_mapped]
+        if (
+          len(geneset_mapped) >= 5
+          and len(geneset_mapped) < 2500
+          and len(term) < 200
+          and term not in terms
+        ):
+          terms.add(term)
+          print(
+            term, '',
+            *geneset_mapped,
+            sep='\t',
+            file=fw,
+          )
+
+@cli.command()
 @click.option('-i', '--input', type=click.Path(exists=True, file_okay=True, path_type=Path), help='GMT file to ingest')
 @click.option('-n', '--name', type=str, required=True, help='Name of the library')
 @click.option('-d', '--description', type=str, default='', help='Description of the library')
 @click.option('-a', '--append', type=bool, is_flag=True, help='Append to the existing library')
-def cli(input, name, description, append):
+def ingest(input, name, description, append):
   from plpy import plpy
   try:
     import_gene_set_library(plpy, input, name, description, append)
