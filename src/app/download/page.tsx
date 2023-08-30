7e338f368d5bc66495d2279d73423ae87162aa7e
giacomomarino <giacomo_marino@brown.edu>
2023-08-30 17:45:01 -0400

    add downloads page
    uncertain if we will offer free downloads yet so just basic template for now

---
+++ src/app/download/page.tsx
@@ -0,0 +1,16 @@
+export default function Download() {
+  return (
+    <div className="prose">
+      <h2 className="title text-xl font-medium mb-3">Downloads</h2>
+      <br></br>
+      <p>
+        This database is updated weekly to extract gene sets automatically from newly published open access PMC articles.
+      </p>
+      <br></br>
+      <p>
+        Developed in <a className='underline cursor' href="https://labs.icahn.mssm.edu/maayanlab/">the Ma&apos;ayan Lab</a>
+      </p>
+      
+    </div>
+  )
+}
