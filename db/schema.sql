64088f86b0baf09a1615eaccb0e7324bc6b93726
Daniel Clarke <u8sand@gmail.com>
2023-09-19 13:16:41 -0400

    Cleanup/update complete schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,24 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: app_private_v2; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA app_private_v2;
+
+
+--
 -- Name: app_public; Type: SCHEMA; Schema: -; Owner: -
 --
 
 CREATE SCHEMA app_public;
+
+
+--
+-- Name: app_public_v2; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA app_public_v2;
 
 
 --
@@ -42,6 +56,20 @@ CREATE EXTENSION IF NOT EXISTS plpython3u WITH SCHEMA pg_catalog;
 --
 
 COMMENT ON EXTENSION plpython3u IS 'PL/Python3U untrusted procedural language';
+
+
+--
+-- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching based on trigrams';
 
 
 --
@@ -75,6 +103,35 @@ CREATE TYPE app_public.gene_set_library_enrich_result AS (
 
 
 --
+-- Name: enrich_result; Type: TYPE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TYPE app_public_v2.enrich_result AS (
+	gene_set_id uuid,
+	odds_ratio double precision,
+	pvalue double precision,
+	adj_pvalue double precision
+);
+
+
+--
+-- Name: TYPE enrich_result; Type: COMMENT; Schema: app_public_v2; Owner: -
+--
+
+COMMENT ON TYPE app_public_v2.enrich_result IS '@foreign key (gene_set_id) references app_public_v2.gene_set (id)';
+
+
+--
+-- Name: paginated_enrich_result; Type: TYPE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TYPE app_public_v2.paginated_enrich_result AS (
+	nodes app_public_v2.enrich_result[],
+	total_count integer
+);
+
+
+--
 -- Name: fishers_exact_result; Type: TYPE; Schema: internal; Owner: -
 --
 
@@ -101,6 +158,41 @@ CREATE TYPE internal.overlap_result AS (
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: background; Type: TABLE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TABLE app_public_v2.background (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    gene_ids jsonb NOT NULL,
+    n_gene_ids integer NOT NULL
+);
+
+
+--
+-- Name: indexed_enrich(app_public_v2.background, uuid[], integer, double precision, double precision, integer, integer); Type: FUNCTION; Schema: app_private_v2; Owner: -
+--
+
+CREATE FUNCTION app_private_v2.indexed_enrich(background app_public_v2.background, gene_ids uuid[], overlap_ge integer DEFAULT 1, pvalue_le double precision DEFAULT 0.05, adj_pvalue_le double precision DEFAULT 0.05, "offset" integer DEFAULT 0, first integer DEFAULT 100) RETURNS app_public_v2.paginated_enrich_result
+    LANGUAGE plpython3u IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  import requests
+  req = requests.post(
+    f"http://enrich:8000/{background['id']}",
+    params=dict(
+      overlap_ge=overlap_ge,
+      pvalue_le=pvalue_le,
+      adj_pvalue_le=adj_pvalue_le,
+      offset=offset,
+      limit=first,
+    ),
+    json=gene_ids,
+  )
+  total_count = req.headers.get('Content-Range').partition('/')[-1]
+  return dict(nodes=req.json(), total_count=total_count)
+$$;
+
 
 --
 -- Name: user_gene_set; Type: TABLE; Schema: app_public; Owner: -
@@ -442,6 +534,24 @@ $$;
 
 
 --
+-- Name: gene_set_library_terms_pmcs_count(app_public.gene_set_library, character varying[]); Type: FUNCTION; Schema: app_public; Owner: -
+--
+
+CREATE FUNCTION app_public.gene_set_library_terms_pmcs_count(gene_set_library app_public.gene_set_library, pmcids character varying[]) RETURNS TABLE(pmc character varying, term character varying, id uuid, count integer)
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select gsp.pmc, gs.term, gs.id, gsl.count
+  from
+    app_public.gene_set_pmc as gsp
+    inner join app_public.gene_set as gs on gs.id = gsp.id
+    inner join app_public.gene_set_length as gsl on gsl.id = gsp.id
+  where
+    gsp.pmc = ANY (pmcids) and
+    gs.library_id = gene_set_library.id;
+$$;
+
+
+--
 -- Name: gene_set_term_search(character varying[]); Type: FUNCTION; Schema: app_public; Owner: -
 --
 
@@ -465,55 +575,6 @@ CREATE FUNCTION app_public.gene_set_term_search_count(terms character varying[])
   from app_public.gene_set gs
   inner join unnest(terms) ut(term) on gs.term ilike ('%' || ut.term || '%')
   inner join app_public.gene_set_length gsl on gsl.id = gs.id;
-$$;
-
-
---
--- Name: pmc_info; Type: TABLE; Schema: app_public; Owner: -
---
-
-CREATE TABLE app_public.pmc_info (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    pmcid character varying NOT NULL,
-    title character varying,
-    yr integer,
-    doi character varying
-);
-
-
---
--- Name: TABLE pmc_info; Type: COMMENT; Schema: app_public; Owner: -
---
-
-COMMENT ON TABLE app_public.pmc_info IS '@foreignKey (pmcid) references app_public.gene_set_pmc (pmc)';
-
-
---
--- Name: get_pmc_info_by_ids(character varying[]); Type: FUNCTION; Schema: app_public; Owner: -
---
-
-CREATE FUNCTION app_public.get_pmc_info_by_ids(pmcids character varying[]) RETURNS SETOF app_public.pmc_info
-    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
-    AS $$
-  select *
-  from app_public.pmc_info
-  where pmcid = ANY (pmcIds);
-$$;
-
-
---
--- Name: terms_pmcs(character varying[]); Type: FUNCTION; Schema: app_public; Owner: -
---
-
-CREATE FUNCTION app_public.terms_pmcs(pmcids character varying[]) RETURNS TABLE(pmc character varying, term character varying, id uuid)
-    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
-    AS $$
-  select gsp.pmc, gs.term, gs.id
-  from
-    app_public.gene_set_pmc as gsp
-    inner join app_public.gene_set as gs on gs.id = gsp.id
-    --inner join app_public.gene_set_length as gsl on gsl.gene_set_id = gsp.id
-  where gsp.pmc = ANY (pmcids);
 $$;
 
 
@@ -549,24 +610,225 @@ $$;
 
 
 --
--- Name: fishers_exact(uuid[], bigint[], bigint[], bigint[], bigint[], bigint, double precision, double precision, double precision); Type: FUNCTION; Schema: internal; Owner: -
+-- Name: user_gene_set; Type: TABLE; Schema: app_public_v2; Owner: -
 --
 
-CREATE FUNCTION internal.fishers_exact(ids uuid[], a bigint[], b bigint[], c bigint[], d bigint[], n bigint, fdr double precision DEFAULT 0.05, pvalue_less_than double precision DEFAULT 0.05, adj_pvalue_less_than double precision DEFAULT 0.05) RETURNS SETOF internal.fishers_exact_result
-    LANGUAGE plpython3u IMMUTABLE STRICT PARALLEL SAFE
+CREATE TABLE app_public_v2.user_gene_set (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    genes character varying[],
+    description character varying DEFAULT ''::character varying,
+    created timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: add_user_gene_set(character varying[], character varying); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.add_user_gene_set(genes character varying[], description character varying DEFAULT ''::character varying) RETURNS app_public_v2.user_gene_set
+    LANGUAGE sql SECURITY DEFINER
     AS $$
-  import sigcomlite
-  yield from sigcomlite.fishers_exact(
-    ids=ids,
-    a=a,
-    b=b,
-    c=c,
-    d=d,
-    n=n,
-    fdr=fdr,
-    pvalue_less_than=pvalue_less_than,
-    adj_pvalue_less_than=adj_pvalue_less_than,
-  )
+  insert into app_public_v2.user_gene_set (genes, description)
+  select
+    (
+      select array_agg(ug.gene order by ug.gene)
+      from unnest(add_user_gene_set.genes) ug(gene)
+    ) as genes,
+    add_user_gene_set.description
+  returning *;
+$$;
+
+
+--
+-- Name: background_enrich(app_public_v2.background, character varying[], integer, double precision, double precision, integer, integer); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.background_enrich(background app_public_v2.background, genes character varying[], overlap_ge integer DEFAULT 1, pvalue_le double precision DEFAULT 0.05, adj_pvalue_le double precision DEFAULT 0.05, "offset" integer DEFAULT 0, first integer DEFAULT 100) RETURNS app_public_v2.paginated_enrich_result
+    LANGUAGE sql IMMUTABLE STRICT SECURITY DEFINER PARALLEL SAFE
+    AS $$
+  select r.*
+  from app_private_v2.indexed_enrich(
+    background_enrich.background,
+    (select array_agg(gene_id) from app_public_v2.gene_map(genes) gm),
+    background_enrich.overlap_ge,
+    background_enrich.pvalue_le,
+    background_enrich.adj_pvalue_le,
+    background_enrich."offset",
+    background_enrich."first"
+  ) r;
+$$;
+
+
+--
+-- Name: background_overlap(app_public_v2.background, character varying[], integer); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.background_overlap(background app_public_v2.background, genes character varying[], overlap_greater_than integer DEFAULT 0) RETURNS TABLE(gene_set_id uuid, n_overlap_gene_ids integer, n_gs_gene_ids integer)
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $$
+  select
+    gs.id as gene_set_id,
+    count(ig.gene_id) as n_overlap_gene_ids,
+    gs.n_gene_ids as n_gs_gene_ids
+  from
+    (
+      select distinct g.gene_id::text
+      from app_public_v2.gene_map(background_overlap.genes) g
+    ) ig
+    inner join app_public_v2.gene_set gs on gs.gene_ids ? ig.gene_id
+  group by gs.id
+  having count(ig.gene_id) > background_overlap.overlap_greater_than;
+$$;
+
+
+--
+-- Name: gene_set; Type: TABLE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TABLE app_public_v2.gene_set (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    term character varying NOT NULL,
+    gene_ids jsonb NOT NULL,
+    n_gene_ids integer NOT NULL
+);
+
+
+--
+-- Name: enrich_result_gene_set(app_public_v2.enrich_result); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.enrich_result_gene_set(enrich_result app_public_v2.enrich_result) RETURNS app_public_v2.gene_set
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $$
+  select gs.*
+  from app_public_v2.gene_set gs
+  where gs.id = enrich_result.gene_set_id;
+$$;
+
+
+--
+-- Name: gene_map(character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.gene_map(genes character varying[]) RETURNS TABLE(gene_id uuid, gene character varying)
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select g.id as gene_id, ug.gene as gene
+  from unnest(gene_map.genes) ug(gene)
+  inner join app_public_v2.gene g on g.symbol = ug.gene or g.synonyms ? ug.gene;
+$$;
+
+
+--
+-- Name: gene_set_gene_search(character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.gene_set_gene_search(genes character varying[]) RETURNS SETOF app_public_v2.gene_set
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select distinct gs.*
+  from
+    app_public_v2.gene_map(genes) g
+    inner join app_public_v2.gene_set gs on gs.gene_ids ? g.gene_id::text;
+$$;
+
+
+--
+-- Name: gene; Type: TABLE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TABLE app_public_v2.gene (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    symbol character varying NOT NULL,
+    synonyms jsonb DEFAULT '{}'::jsonb
+);
+
+
+--
+-- Name: gene_set_genes(app_public_v2.gene_set); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.gene_set_genes(gene_set app_public_v2.gene_set) RETURNS SETOF app_public_v2.gene
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select g.*
+  from app_public_v2.gene g
+  where gene_set_genes.gene_set.gene_ids ? g.id::text;
+$$;
+
+
+--
+-- Name: gene_set_overlap(app_public_v2.gene_set, character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.gene_set_overlap(gene_set app_public_v2.gene_set, genes character varying[]) RETURNS SETOF app_public_v2.gene
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $$
+  select distinct g.*
+  from app_public_v2.gene_map(gene_set_overlap.genes) gm
+  inner join app_public_v2.gene g on g.id = gm.gene_id
+  where gene_set.gene_ids ? gm.gene_id::text;
+$$;
+
+
+--
+-- Name: gene_set_term_search(character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.gene_set_term_search(terms character varying[]) RETURNS SETOF app_public_v2.gene_set
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select distinct gs.*
+  from app_public_v2.gene_set gs
+  inner join unnest(terms) ut(term) on gs.term ilike ('%' || ut.term || '%');
+$$;
+
+
+--
+-- Name: pmc_info; Type: TABLE; Schema: app_public_v2; Owner: -
+--
+
+CREATE TABLE app_public_v2.pmc_info (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    pmcid character varying NOT NULL,
+    title character varying,
+    yr integer,
+    doi character varying
+);
+
+
+--
+-- Name: TABLE pmc_info; Type: COMMENT; Schema: app_public_v2; Owner: -
+--
+
+COMMENT ON TABLE app_public_v2.pmc_info IS '@foreignKey (pmcid) references app_public_v2.gene_set_pmc (pmc)';
+
+
+--
+-- Name: get_pmc_info_by_ids(character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.get_pmc_info_by_ids(pmcids character varying[]) RETURNS SETOF app_public_v2.pmc_info
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select *
+  from app_public_v2.pmc_info
+  where pmcid = ANY (pmcIds);
+$$;
+
+
+--
+-- Name: terms_pmcs_count(character varying[]); Type: FUNCTION; Schema: app_public_v2; Owner: -
+--
+
+CREATE FUNCTION app_public_v2.terms_pmcs_count(pmcids character varying[]) RETURNS TABLE(pmc character varying, term character varying, id uuid, count integer)
+    LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE
+    AS $$
+  select gsp.pmc, gs.term, gs.id, gs.n_gene_ids as count
+  from
+    app_public_v2.gene_set_pmc as gsp
+    inner join app_public_v2.gene_set as gs on gs.id = gsp.id
+  where gsp.pmc = ANY (pmcids);
 $$;
 
 
@@ -838,11 +1100,65 @@ COMMENT ON VIEW app_public.pmc IS '@foreignKey (pmc) references app_public.gene_
 
 
 --
+-- Name: pmc_info; Type: TABLE; Schema: app_public; Owner: -
+--
+
+CREATE TABLE app_public.pmc_info (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    pmcid character varying NOT NULL,
+    title character varying,
+    yr integer,
+    doi character varying
+);
+
+
+--
+-- Name: TABLE pmc_info; Type: COMMENT; Schema: app_public; Owner: -
+--
+
+COMMENT ON TABLE app_public.pmc_info IS '@foreignKey (pmcid) references app_public.gene_set_pmc (pmc)';
+
+
+--
+-- Name: gene_set_pmc; Type: MATERIALIZED VIEW; Schema: app_public_v2; Owner: -
+--
+
+CREATE MATERIALIZED VIEW app_public_v2.gene_set_pmc AS
+ SELECT gs.id,
+    regexp_replace((gs.term)::text, '^(^PMC\d+)(.*)$'::text, '\1'::text) AS pmc
+   FROM app_public_v2.gene_set gs
+  WITH NO DATA;
+
+
+--
+-- Name: MATERIALIZED VIEW gene_set_pmc; Type: COMMENT; Schema: app_public_v2; Owner: -
+--
+
+COMMENT ON MATERIALIZED VIEW app_public_v2.gene_set_pmc IS '@foreignKey (id) references app_public_v2.gene_set (id)';
+
+
+--
+-- Name: pmc; Type: VIEW; Schema: app_public_v2; Owner: -
+--
+
+CREATE VIEW app_public_v2.pmc AS
+ SELECT DISTINCT gene_set_pmc.pmc
+   FROM app_public_v2.gene_set_pmc;
+
+
+--
+-- Name: VIEW pmc; Type: COMMENT; Schema: app_public_v2; Owner: -
+--
+
+COMMENT ON VIEW app_public_v2.pmc IS '@foreignKey (pmc) references app_public_v2.gene_set_pmc (pmc)';
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.schema_migrations (
-    version character varying(255) NOT NULL
+    version character varying(128) NOT NULL
 );
 
 
@@ -943,6 +1259,70 @@ ALTER TABLE ONLY app_public.user_gene_set
 
 
 --
+-- Name: background background_pkey; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.background
+    ADD CONSTRAINT background_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: gene gene_pkey; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.gene
+    ADD CONSTRAINT gene_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: gene_set gene_set_pkey; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.gene_set
+    ADD CONSTRAINT gene_set_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: gene_set gene_set_term_key; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.gene_set
+    ADD CONSTRAINT gene_set_term_key UNIQUE (term);
+
+
+--
+-- Name: gene gene_symbol_key; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.gene
+    ADD CONSTRAINT gene_symbol_key UNIQUE (symbol);
+
+
+--
+-- Name: pmc_info pmc_info_pkey; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.pmc_info
+    ADD CONSTRAINT pmc_info_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: pmc_info pmc_info_pmcid_key; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.pmc_info
+    ADD CONSTRAINT pmc_info_pmcid_key UNIQUE (pmcid);
+
+
+--
+-- Name: user_gene_set user_gene_set_pkey; Type: CONSTRAINT; Schema: app_public_v2; Owner: -
+--
+
+ALTER TABLE ONLY app_public_v2.user_gene_set
+    ADD CONSTRAINT user_gene_set_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -993,6 +1373,13 @@ CREATE INDEX gene_set_library_id_idx ON app_public.gene_set USING btree (library
 
 
 --
+-- Name: gene_set_pmc_id; Type: INDEX; Schema: app_public; Owner: -
+--
+
+CREATE INDEX gene_set_pmc_id ON app_public.gene_set_pmc USING btree (id);
+
+
+--
 -- Name: gene_set_pmc_id_idx; Type: INDEX; Schema: app_public; Owner: -
 --
 
@@ -1032,6 +1419,55 @@ CREATE INDEX gene_synonym_gene_id_idx ON app_public.gene_synonym USING btree (ge
 --
 
 CREATE INDEX idx_gene_set_length ON app_public.gene_set_length USING btree (id);
+
+
+--
+-- Name: background_gene_ids_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX background_gene_ids_idx ON app_public_v2.background USING gin (gene_ids);
+
+
+--
+-- Name: gene_set_gene_ids_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX gene_set_gene_ids_idx ON app_public_v2.gene_set USING gin (gene_ids);
+
+
+--
+-- Name: gene_set_pmc_id_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX gene_set_pmc_id_idx ON app_public_v2.gene_set_pmc USING btree (id);
+
+
+--
+-- Name: gene_set_pmc_id_pmc_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE UNIQUE INDEX gene_set_pmc_id_pmc_idx ON app_public_v2.gene_set_pmc USING btree (id, pmc);
+
+
+--
+-- Name: gene_set_pmc_pmc_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX gene_set_pmc_pmc_idx ON app_public_v2.gene_set_pmc USING btree (pmc);
+
+
+--
+-- Name: gene_set_term_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX gene_set_term_idx ON app_public_v2.gene_set USING gin (term public.gin_trgm_ops);
+
+
+--
+-- Name: gene_synonyms_idx; Type: INDEX; Schema: app_public_v2; Owner: -
+--
+
+CREATE INDEX gene_synonyms_idx ON app_public_v2.gene USING gin (synonyms);
 
 
 --
@@ -1097,4 +1533,6 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20230828144957'),
     ('20230830165721'),
     ('20230830171356'),
-    ('20230830210011');
+    ('20230830210011'),
+    ('20230906154745'),
+    ('20230918153613');
