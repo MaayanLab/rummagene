406499ba09e13398c83e8080fe2f745fec0032c6
Daniel Clarke <u8sand@gmail.com>
2023-09-19 18:04:19 -0400

    Add a way to update the background

--- helper.py
+++ helper.py
@@ -260,5 +260,34 @@ def create_release(publications):
   plpy.execute('refresh materialized view app_private_v2.pmc_stats;')
   plpy.con.commit()
 
+@cli.command()
+def update_background():
+  ''' A background is tied to a complete set of genes across all gene sets
+  but also to a computed index in the enrich API. This function creates a
+  new one, and drops the old one after ensuring the index is ready.
+  '''
+  import requests
+  from plpy import plpy
+  # record current backgrounds
+  current_backgrounds = [row['id'] for row in plpy.cursor('select id from app_public_v2.background')]  # create updated background
+  new_background, = plpy.cursor('''
+    insert into app_public_v2.background (gene_ids, n_gene_ids)
+    select
+      jsonb_object_agg(distinct gsg.gene_id, null) as gene_ids,
+      count(distinct gsg.gene_id) as n_gene_ids
+    from app_public_v2.gene_set gs, jsonb_each(gs.gene_ids) gsg(gene_id, nil)
+    returning id;
+  ''')
+  # trigger index creation for the new background
+  requests.get(f"http://127.0.0.1:8000/{new_background}")
+  # remove old backgrounds
+  plpy.execute(
+    plpy.prepare('delete from app_public_v2.background where id = any($1)', ['uuid[]']),
+    [current_backgrounds]
+  )
+  # remove index for the old background
+  for current_background in current_backgrounds:
+    requests.delete(f"http://127.0.0.1:8000/{current_background}")
+
 if __name__ == '__main__':
   cli()
