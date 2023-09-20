fc8977fd50eff3ac06735f525efe479a9676a529
Daniel Clarke <u8sand@gmail.com>
2023-09-20 16:06:22 -0400

    Fixup db docker-entrypoint

--- db/docker-entrypoint-initdb.d/search-paths.sh
+++ db/docker-entrypoint-initdb.d/search-paths.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 cat >> /var/lib/postgresql/data/postgresql.conf << EOF
-search_path = '"$user", public, app_public, app_private'
+search_path = '"$user", public, app_public_v2, app_private_v2'
 EOF
