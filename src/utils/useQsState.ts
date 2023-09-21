0ac7a464c354208a912dccdc56bae432d782bfda
Daniel Clarke <u8sand@gmail.com>
2023-09-21 13:35:23 -0400

    Fixup useQsState

--- src/utils/useQsState.ts
+++ src/utils/useQsState.ts
@@ -1,25 +1,31 @@
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
-export function useQsState<T>(paramName: string, initialState: T, opts = { encode: JSON.stringify, decode: JSON.parse }) {
+const identity = <T>(item: T) => item
+type Codec<T> = {
+  encode: (decoded: T) => string,
+  decode: (encoded: string) => T,
+}
+export const JsonCodec = { encode: JSON.stringify, decode: JSON.parse }
+export const StringCodec = { encode: identity, decode: identity }
+
+export function useQsState<T>(paramName: string, initialState: T, codec_?: Codec<T>) {
+  const codec = codec_ !== undefined ? codec_ : (typeof initialState === 'string') ? StringCodec : JsonCodec
   const router = useRouter()
+  const searchParams = useSearchParams()
   const [paramValue, setParamValue] = React.useState(initialState)
   React.useEffect(() => {
-    const listener = () => {
-      const url = new URLSearchParams(window.location.search)
-      const value = url.get(paramName)
-      if (value !== null) {
-        setParamValue(opts.decode(value))
-      }
+    const value = searchParams?.get(paramName)
+    if (value === undefined || value === null) {
+      setParamValue(initialState)
+    } else {
+      setParamValue(codec.decode(value))
     }
-    listener()
-    window.addEventListener('popstate', listener)
-    return () => {window.removeEventListener('popstate', listener)}
-  }, [paramName, opts])
+  }, [searchParams, paramName, initialState, codec])
   const setParam = React.useCallback((paramValue: T) => {
     const url = new URLSearchParams(window.location.search)
-    url.set(paramName, opts.encode(paramValue))
+    url.set(paramName, codec.encode(paramValue))
     router.replace(window.location.pathname + '?' + url.toString(), { scroll: false })
-  }, [router, paramName, opts])
+  }, [router, paramName, codec])
   return [paramValue, setParam] as const
 }
