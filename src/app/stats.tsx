d7f70c81e84fd23275e5d22a176eb3dd258afa76
giacomomarino <giacomo_marino@brown.edu>
2023-12-07 11:13:01 -0500

    update graphql queries for species

--- src/app/stats.tsx
+++ src/app/stats.tsx
@@ -6,23 +6,19 @@ export default function Stats({
   bold,
   show_sets_analyzed,
   show_gene_sets,
-  show_pmcs,
-  show_publications,
+  show_gses
 }: {
   bold?: boolean,
   show_sets_analyzed?: boolean,
   show_gene_sets?: boolean,
-  show_pmcs?: boolean,
-  show_publications?: boolean
+  show_gses?: boolean,
 }) {
   const { data } = useStatsQuery({ pollInterval: 60000 })
 
   if (show_gene_sets) {
     return (data?.geneSets?.totalCount !== undefined && show_gene_sets) ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.geneSets.totalCount)}</span>&nbsp;gene sets</> : <span className='loading'>loading</span>
-  } else if (show_pmcs){
-    return (data?.pmcs?.totalCount !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.pmcs.totalCount)}</span>&nbsp; GEO studies</> : <span className='loading'>loading</span>)
-  } else if (show_publications) {
-    return (data?.pmcStats?.nPublicationsProcessed !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.pmcStats.nPublicationsProcessed)}</span>&nbsp;GEO studies</> : <span className='loading'>loading</span>) 
+  } else if (show_gses){
+    return (data?.gses?.totalCount !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.gses.totalCount)}</span>&nbsp;GEO studies</> : <span className='loading'>loading</span>)
   } else if (show_sets_analyzed) {
     return (data?.userGeneSets?.totalCount !== undefined ? <><span className={classNames({'font-bold': bold})}>{Intl.NumberFormat("en-US", {}).format(data.userGeneSets.totalCount)}</span>&nbsp;sets analyzed</> : <span className='loading'>loading</span>)
   } else{
