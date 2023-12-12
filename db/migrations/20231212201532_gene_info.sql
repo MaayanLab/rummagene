e1956e0daa866f5b327a92a6f25dc47ee0c88e40
Daniel J. B. Clarke <u8sand@gmail.com>
2023-12-12 17:54:43 -0500

    Fixup gene info migration

--- db/migrations/20231212201532_gene_info.sql
+++ db/migrations/20231212201532_gene_info.sql
@@ -1,9 +1,11 @@
 -- migrate:up
 alter table app_public_v2.gene
+add column ncbi_gene_id integer,
 add column description varchar,
 add column summary text;
 
 -- migrate:down
 alter table app_public_v2.gene
+drop column ncbi_gene_id integer,
 drop column description,
 drop column summary;
