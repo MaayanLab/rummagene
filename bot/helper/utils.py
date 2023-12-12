8d4d12fc256e270dcd0788df5d42161a2bd5b771
Daniel J. B. Clarke <u8sand@gmail.com>
2023-12-12 15:46:15 -0500

    copy_from_records partial insertion support

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -3,7 +3,13 @@ import typing as t
 
 FileDescriptor = t.Union[int, str]
 
-def copy_from_tsv(conn: 'psycopg2.connection', table: str, columns: list[str], r: FileDescriptor):
+def copy_from_tsv(
+  conn: 'psycopg2.connection',
+  table: str,
+  columns: list[str],
+  r: FileDescriptor,
+  on_conflict_update: t.Optional[t.Tuple[str]] = None,
+):
   ''' Copy from a file descriptor into a postgres database table through as psycopg2 connection object
   :param con: The psycopg2.connect object
   :param r: An file descriptor to be opened in read mode
@@ -14,16 +20,38 @@ def copy_from_tsv(conn: 'psycopg2.connection', table: str, columns: list[str], r
   with conn.cursor() as cur:
     with os.fdopen(r, 'rb', buffering=0, closefd=True) as fr:
       columns = fr.readline().strip().split(b'\t')
-      cur.copy_expert(
-        sql=f'''
-        COPY {table} ({",".join(f'"{c.decode()}"' for c in columns)})
-        FROM STDIN WITH CSV DELIMITER E'\\t'
-        ''',
-        file=fr,
-      )
+      if on_conflict_update:
+        cur.copy_expert(
+          sql=f'''
+          CREATE TABLE {table+'_tmp'} as table {table} WITH NO DATA;
+          COPY {table+'_tmp'} ({",".join(f'"{c.decode()}"' for c in columns)})
+          FROM STDIN WITH CSV DELIMITER E'\\t';
+          INSERT INTO {table} ({",".join(f'"{c.decode()}"' for c in columns)})
+          SELECT *
+          FROM {table+'_tmp'}
+          ON CONFLICT ({",".join(f'"{c}"' for c in on_conflict_update)})
+          DO UPDATE SET {", ".join(f'"{c}" = EXCLUDED."{c}"' for c in map(bytes.decode, columns) if c not in on_conflict_update)};
+          DROP TABLE {table+'_tmp'};
+          ''',
+          file=fr,
+        )
+      else:
+        cur.copy_expert(
+          sql=f'''
+          COPY {table} ({",".join(f'"{c.decode()}"' for c in columns)})
+          FROM STDIN WITH CSV DELIMITER E'\\t'
+          ''',
+          file=fr,
+        )
     conn.commit()
 
-def copy_from_records(conn: 'psycopg2.connection', table: str, columns: list[str], records: t.Iterable[dict]):
+def copy_from_records(
+  conn: 'psycopg2.connection',
+  table: str,
+  columns: list[str],
+  records: t.Iterable[dict],
+  on_conflict_update: t.Optional[t.Tuple[str]] = None,
+):
   ''' Copy from records into a postgres database table through as psycopg2 connection object.
   This is done by constructing a unix pipe, writing the records with csv writer
    into the pipe while loading from the pipe into postgres at the same time.
@@ -38,7 +66,7 @@ def copy_from_records(conn: 'psycopg2.connection', table: str, columns: list[str
   #  another thread
   rt = threading.Thread(
     target=copy_from_tsv,
-    args=(conn, table, columns, r,),
+    args=(conn, table, columns, r, on_conflict_update),
   )
   rt.start()
   try:
