c6399f3f7911158938de3006c669ae9fdf204ded
Daniel J. B. Clarke <u8sand@gmail.com>
2024-01-05 11:52:24 -0500

    Add table-mining-clean-with-desc download

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -26,6 +26,15 @@ export default function Download() {
             </div>
           </div>
         </a>
+        <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummagene/table-mining-clean-with-desc.gmt.gz" download="table-mining-clean-with-desc.gmt.gz">
+          <div className="stat gap-2">
+            <div className="stat-title">table-mining-clean-with-desc.gmt.gz</div>
+            <div className="stat-value">642,389 gene sets</div>
+            <div className="stat-desc whitespace-normal">
+              <span className="whitespace-nowrap">253MB compressed</span>, <span className="whitespace-nowrap">752MB uncompressed</span>, <span className="whitespace-nowrap">Last Updated Fri Jan 5 2024</span>
+            </div>
+          </div>
+        </a>
         <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummagene/table-mining-clean.gmt.gz" download="table-mining-clean.gmt.gz">
           <div className="stat gap-2">
             <div className="stat-title">table-mining-clean.gmt.gz</div>
