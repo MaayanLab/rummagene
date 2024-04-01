c61f17bbb78cd12895c2e6780814ca93bddd34f6
Daniel Clarke <u8sand@gmail.com>
2024-04-01 19:14:31 -0400

    [submit]: Add cfde-gskg

---
+++ src/app/gene_set/[id]/submit/cfde-gskg/route.ts
@@ -0,0 +1,29 @@
+import { ViewGeneSetDocument, ViewGeneSetQuery, ViewGeneSetQueryVariables } from "@/graphql"
+import { getClient } from "@/lib/apollo/client"
+import { redirect } from 'next/navigation'
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const client = getClient()
+  const geneSet = await client.query<ViewGeneSetQuery, ViewGeneSetQueryVariables>({
+    query: ViewGeneSetDocument,
+    variables: {
+      id: params.id,
+    },
+  })
+  if (!geneSet.data.geneSet) return new Response(JSON.stringify({error: 'Not Found'}), { status: 404 })
+  const formData = new FormData()
+  formData.append('list', geneSet.data.geneSet.genes.nodes.map(gene => gene.symbol).join('\n'))
+  formData.append('description', `Rummagene Gene Set ${params.id}`)
+  const req = await fetch('https://maayanlab.cloud/Enrichr/api/addList', {
+    headers: {
+      'Accept': 'application/json',
+    },
+    method: 'POST',
+    body: formData,
+  })
+  const res = await req.json()
+  if (!res.shortId) return new Response(JSON.stringify({error: 'Failed to Register Gene Set'}), { status: 500 })
+  const searchParams = new URLSearchParams()
+  searchParams.append('q', JSON.stringify({"userListId":res.userListId,"search":true}))
+  redirect(`https://cfde-gskg.dev.maayanlab.cloud/?${searchParams.toString()}`)
+}
