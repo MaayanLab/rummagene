2d2d9d2e9cd95a235f9ecfcff19820aaaf746564
Daniel Clarke <u8sand@gmail.com>
2024-04-23 11:05:26 -0400

    More improvements to the email

--- bot/helper/cli/compose_emails.py
+++ bot/helper/cli/compose_emails.py
@@ -1,3 +1,4 @@
+import re
 import json
 import click
 import pandas as pd
@@ -7,6 +8,20 @@ from pathlib import Path
 from textwrap import dedent
 from helper.cli import cli
 
+class Context(dict):
+  def register(self, fn):
+    self[fn.__name__] = fn
+ctx = Context()
+ctx.register(quote)
+
+@ctx.register
+def maybe_the(s):
+  if re.match(r'^(\w+\s)?journal', s, re.IGNORECASE):
+    return f"the {s}"
+  else:
+    return s
+
+@ctx.register
 def human_list(L):
   if len(L) == 0:
     return ''
@@ -22,22 +37,19 @@ def human_list(L):
 
 email_template = jinja2.Template(dedent('''
   {%- set single_gene_set = articles|length == 1 and articles[0].terms|length == 1 %}
+  {%- set single_article = articles|length == 1 %}
   To: {{ email }}
-  Subject: We found {% if single_gene_set %}a gene set{% else %}gene sets{% endif %} in your recent paper
+  Subject: Adding your recently published gene set{% if not single_gene_set %}s{% endif %} to the Rummagene database
 
   Dear Dr. {{ surname }},
 
-  {%- set single_article = articles|length == 1 %}
+  Congratulations on your recent publication{% if not single_article %}s{% endif %} in {{ maybe_the(human_list(journals)) }} ({% for article in articles %}{% if not loop.first %}, {% endif %}<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/{{ article.pmc }}">{{ article.pmc }}</a>{% endfor %}).
 
-  Congratulations for your recent publication{% if not single_article %}s{% endif %} in {{ human_list(journals) }}.
-  
-  {%- set single_gene_set = articles|length == 1 and articles[0].terms|length == 1 %}
-
-  We crawled the tables & supporting materials and found {% if single_gene_set %}a unique gene set{% else %}unique gene sets{% endif %} that we added to <a href="https://rummagene.com">Rummagene</a>.
+  We crawled the tables and supporting materials of the paper{% if single_article %}s{% endif %}, and found {% if single_gene_set %}a unique gene set{% else %}unique gene sets{% endif %} in it. We added this gene set to <a href="https://rummagene.com">Rummagene</a>, a new database and search engine that mines gene sets from tables and supporting materials of biomedical research publications.
 
   {%- if single_gene_set %}
 
-  You can further analyze the gene set by submitting them for enrichment analysis with the following tools:
+  You can further analyze the gene set by submitting it for enrichment analysis with the following tools:
 
   <a href="https://rummagene.com/gene_set/{{ quote(articles[0].terms[0]) }}/submit/rummagene">Rummagene</a> - find gene sets in other papers' supporting materials that are similar to your gene set
   {#<a href="https://rummagene.com/gene_set/{{ quote(articles[0].terms[0]) }}/submit/rummageo">RummaGEO</a> - find matching up/down gene sets extracted from the NCBI GEO database similar to your gene set#}
@@ -76,7 +88,7 @@ email_template = jinja2.Template(dedent('''
   </table>
   {%- endif %}
 
-  We hope that you will find this resource useful.
+  We hope that these links are helpful to advancing your research. Please let us know if you have any questions or concerns.
   {%- endif %}
 
   Sincerely,
@@ -105,6 +117,6 @@ def compose_emails(input: Path | str):
   email_article_authors['journals'] = email_article_authors['articles'].apply(lambda A: sorted({a['journal_title'] for a in A}))
   for email, record in email_article_authors.iterrows():
     print(
-      email_template.render(dict(record.to_dict(), email=email, human_list=human_list, quote=quote)),
+      email_template.render(**record.to_dict(), email=email, **ctx),
       end='\n\n\n=====\n\n\n',
     )
