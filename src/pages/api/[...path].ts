68404f4d76a7804c7b4c891ba828cc9cc1e3d3ea
Daniel Clarke <u8sand@gmail.com>
2024-12-18 11:43:08 -0500

    Support GET usage of graphql endpoint

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -2,8 +2,29 @@ import postgraphile from '@/lib/postgraphile'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let { method, body } = req
   res.statusCode = 200
-  await new Promise<void>((resolve, reject) => postgraphile(req, res, (err) => { if (err) { reject(err) } else { resolve() } }))
+  if (method === 'GET') {
+    req.method = 'POST'
+    req.body = {
+      query: req.query.query,
+      operationName: req.query.operationName,
+      variables: req.query.variables,
+    }
+  }
+  await new Promise<void>(
+    (resolve, reject) => postgraphile(req, res,
+      (err) => {
+        req.method = method
+        req.body = body
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      }
+    )
+  )
   res.end()
 }
 
